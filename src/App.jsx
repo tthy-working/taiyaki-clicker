771da92e9a4b17b95881e59bf5de8b7be8ea1222
anfebladi <anfebladi4@gmail.com>
2025-11-07 14:33:34 -0800

    howdy

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import Home from "./Components/Home.jsx";
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      < Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import reactLogo from '../assets/react.svg'
+
+
+
+export default function Home() {
+
+
+    return (
+        <>
+            <div className="container-fluid">
+                <img src={reactLogo} className="logo" alt="Vite logo" />
+            </div>
+        </>
+    )
+}
