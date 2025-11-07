b693e765af71749326a7443aab4bf285cd37505a
anfebladi <anfebladi4@gmail.com>
2025-11-07 15:12:11 -0800

    home page created

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import reactLogo from '../assets/react.svg'
+
 
 
 
@@ -6,10 +6,30 @@ export default function Home() {
 
 
     return (
-        <>
-            <div className="container-fluid">
-                <img src={reactLogo} className="logo" alt="Vite logo" />
-            </div>
-        </>
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+      <div className="text-center">
+        <div className="mb-4">
+          <img 
+            src="/taiyaki.png"
+            alt="Taiyaki"
+            className="img-fluid rounded shadow-lg"
+            style={{ 
+              maxWidth: '300px',
+              width: '100%'
+            }}
+            onError={(e) => {
+              e.target.src = 'https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?w=400&h=400&fit=crop';
+            }}
+          />
+        </div>
+        
+        <button 
+          className="btn btn-primary btn-lg px-5 py-3 shadow"
+          style={{ fontSize: '1.5rem' }}
+        >
+          Play Now
+        </button>
+      </div>
+    </div>
     )
 }
